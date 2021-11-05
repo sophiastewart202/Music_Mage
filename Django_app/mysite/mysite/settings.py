c4d4278e9fd1dee2e8913319d2652fa190ae6891
Sophia <sophia.stewart202@protonmail.com>
2021-11-04 23:36:29 -0400

    misc2

--- Django_app/mysite/mysite/settings.py
+++ Django_app/mysite/mysite/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f#p(ub+mnlalb3!*b!lk879m%1*d&z=!et^wf7uc*bl%3d$&ld'
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
