6924d6c2e1d7d6112662c37a6080b31580fe75c3
Sophia <sophia.stewart202@protonmail.com>
2021-11-04 23:29:56 -0400

    misc

--- Django_app/mysite/mysite/settings.py
+++ Django_app/mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from pytz import common_timezones
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f#p(ub+mnlalb3!*b!lk879m%1*d&z=!et^wf7uc*bl%3d$&ld'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
